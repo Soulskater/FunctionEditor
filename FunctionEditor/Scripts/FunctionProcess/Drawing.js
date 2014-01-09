e2325302f7a2aa60cec05b9e96e14222d2ab7fd3
Soulskater <gmeszaros007@gmail.com>
2014-01-09 18:15:50 +0100

    New Parser

--- FunctionEditor/Scripts/FunctionProcess/Drawing.js
+++ FunctionEditor/Scripts/FunctionProcess/Drawing.js
@@ -57,36 +57,43 @@
     };
 
     var drawPoint = function (point) {
-
         ctx.lineTo(point.x + 0.1, point.y + 0.1);
+    }
+
+    //
+    //
+    var _createDatasource = function (input)
+    {
+        var data = [];
+        for (var x = 0 - defaults.size.width / 2; x <= defaults.size.width / 2; x++) {
+            var x0 = x / defaults.scaleX;
+            var func = input.replace(/x/g, x0);
+            var y = parser.parseText(func);
+            if (y != Infinity && !isNaN(y))
+                data.push({
+                    x: origo.x + x,
+                    y: origo.y - (y * defaults.scaleX)
+                });
+        }
+        drawFunction(data, defaults.colors[0])
     }
 
     //
     //datasource is an array of points
     var drawFunction = function (datasource, color) {
-        
+
         ctx.beginPath();
         ctx.strokeStyle = color;
         datasource.foreach(function (point) {
             drawPoint(point);
         });
-        //ctx.lineJoin = 'round';
         ctx.stroke();
     }
 
     initCanvas();
-    var testData = [];
-    for (var x = 0 - defaults.size.width / 2; x <= defaults.size.width / 2; x++) {
-        var x0 = x / defaults.scaleX;
-        var y = Math.tan((Math.PI / 180) * x);
-        testData.push({
-            x: origo.x + x,
-            y: origo.y - (y * defaults.scaleX)
-        });
-    }
-    drawFunction(testData, defaults.colors[0])
+    
 
     return {
-
+        createDatasource: _createDatasource
     }
 })();
--- FunctionEditor/Scripts/FunctionProcess/Operators.js
+++ FunctionEditor/Scripts/FunctionProcess/Operators.js
@@ -1,89 +1,122 @@
 ﻿var Operators = (function () {
-    return [
-        {
-            precedence: 0,
-            sign: "+",
-            isOneVariableOp: true,
-            action: function (a, b) {
-                if (isNaN(a) || isNaN(b)) console.error(a + " or " + b + " is not a number!");
-                return parseFloat(a) + parseFloat(b);
-            }
+    return {
+        getOperator: function (value) {
+            return this.primary.firstOrDefault(function (item) { return value == item || item.sign == value.substr(0, item.sign.length); })
         },
-        {
-            precedence: 0,
-            sign: "-",
-            isOneVariableOp: true,
-            action: function (a, b) {
-                if (isNaN(a)) console.error(a + " is not a number!");
-                if (!b || b == "")
-                    return parseFloat(a) * (-1);
-                return parseFloat(a) - parseFloat(b);
+        primary: [
+            {
+                precedence: 0,
+                sign: "+",
+                calculate: function (a, b) {
+                    if (isNaN(a)) console.error(a + " or " + b + " is not a number!");
+                    if (Operators.getOperator(a) && !isNaN(b))
+                        return b;
+                    if (!isNaN(a) && !isNaN(b))
+                        return parseFloat(a) + parseFloat(b);
+
+                    console.log("Error in operator '" + this.sign +"'");
+                }
+            },
+            {
+                precedence: 0,
+                sign: "-",
+                action: function (a, b) {
+                    if (isNaN(a)) console.error(a + " is not a number!");
+                    if (!b || b == "")
+                        return parseFloat(a) * (-1);
+                    return parseFloat(a) - parseFloat(b);
+                }
+            },
+            {
+                precedence: 1,
+                sign: "*",
+                action: function (a, b) {
+                    if (isNaN(a) || isNaN(b)) console.error(a + " or " + b + " is not a number!");
+                    return parseFloat(a) * parseFloat(b);
+                }
+            },
+            {
+                precedence: 1,
+                sign: "/",
+                action: function (a, b) {
+                    if (isNaN(a) || isNaN(b)) console.error(a + " or " + b + " is not a number!");
+                    return parseFloat(a) / parseFloat(b);
+                }
             }
+        //,
+        //{
+        //    precedence: 1,
+        //    sign: "^",
+        //    isOneVariableOp: false,
+        //    action: function (a, b) {
+        //        if (isNaN(a) || isNaN(b)) console.error(a + " or " + b + " is not a number!");
+        //        return Math.pow(parseFloat(a), parseFloat(b));
+        //    }
+        //},
+        //{
+        //    precedence: 1,
+        //    sign: "sqrt",
+        //    isOneVariableOp: true,
+        //    action: function (a, b) {
+        //        if (isNaN(a)) console.error(a + " is not a number!");
+        //        return Math.sqrt(parseFloat(a));
+        //    }
+        //},
+        //{
+        //    precedence: 1,
+        //    sign: "sin",
+        //    isOneVariableOp: true,
+        //    action: function (a, b) {
+        //        if (isNaN(a)) console.error(a + " is not a number!");
+        //        return Math.sin((Math.PI / 180) * a);
+        //    }
+        //},
+        //{
+        //    precedence: 1,
+        //    sign: "cos",
+        //    isOneVariableOp: true,
+        //    action: function (a, b) {
+        //        if (isNaN(a)) console.error(a + " is not a number!");
+        //        return Math.cos((Math.PI / 180) * a);
+        //    }
+        //},
+        //{
+        //    precedence: 1,
+        //    sign: "tan",
+        //    isOneVariableOp: true,
+        //    action: function (a, b) {
+        //        if (isNaN(a)) console.error(a + " is not a number!");
+        //        return Math.tan((Math.PI / 180) * a);
+        //    }
+        //},
+        //{
+        //    precedence: 1,
+        //    sign: "ctg",
+        //    isOneVariableOp: true,
+        //    action: function (a, b) {
+        //        if (isNaN(a)) console.error(a + " is not a number!");
+        //        return 1 / (Math.tan((Math.PI / 180) * a));
+        //    }
+        //}
+        ]
+    };
+})();
+
+var SpecOperators = (function () {
+    return {
+        oBracket: {
+            sign: "(",
         },
-        {
-            precedence: 1,
-            sign: "*",
-            isOneVariableOp: false,
-            action: function (a, b) {
-                if (isNaN(a) || isNaN(b)) console.error(a + " or " + b + " is not a number!");
-                return parseFloat(a) * parseFloat(b);
-            }
+        cBracket: {
+            sign: ")",
         },
-        {
-            precedence: 1,
-            sign: "/",
-            isOneVariableOp: false,
-            action: function (a, b) {
-                if (isNaN(a) || isNaN(b)) console.error(a + " or " + b + " is not a number!");
-                return parseFloat(a) / parseFloat(b);
-            }
-        },
-        {
-            precedence: 1,
-            sign: "^",
-            isOneVariableOp: false,
-            action: function (a, b) {
-                if (isNaN(a) || isNaN(b)) console.error(a + " or " + b + " is not a number!");
-                return Math.pow(parseFloat(a), parseFloat(b));
-            }
-        },
-        {
-            precedence: 1,
-            sign: "sin",
-            isOneVariableOp: true,
-            action: function (a, b) {
-                if (isNaN(a)) console.error(a + " is not a number!");
-                return Math.sin((Math.PI / 180) * a);
-            }
-        },
-        {
-            precedence: 1,
-            sign: "cos",
-            isOneVariableOp: true,
-            action: function (a, b) {
-                if (isNaN(a)) console.error(a + " is not a number!");
-                return Math.cos((Math.PI / 180) * a);
-            }
-        },
-        {
-            precedence: 1,
-            sign: "tan",
-            isOneVariableOp: true,
-            action: function (a, b) {
-                if (isNaN(a)) console.error(a + " is not a number!");
-                return Math.tan((Math.PI / 180) * a);
-            }
-        },
-        {
-            precedence: 1,
-            sign: "ctg",
-            isOneVariableOp: true,
-            action: function (a, b) {
-                if (isNaN(a)) console.error(a + " is not a number!");
-                return 1/(Math.tan((Math.PI / 180) * a));
-            }
+        getOperator: function (text) {
+            if (text.substr(0, this.oBracket.sign.length) == this.oBracket.sign)
+                return this.oBracket;
+            if (text.substr(0, this.cBracket.sign.length) == this.cBracket.sign)
+                return this.cBracket; 
         }
-    ];
+    }
 })();
 
 var Operand = function () {
